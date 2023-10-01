22bf70c670aaeb42f43560bca38c7652a15d385a
Kazi Islam <kazichaska@gmail.com>
2023-10-01 11:58:49 -0500

    java development env template

---
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technews/TechNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
